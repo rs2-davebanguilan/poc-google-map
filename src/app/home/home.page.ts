6158b926fe0e9546e4fbae5c2976fce8778cf73d
rs2-davebanguilan <dave.banguilan@rs2.com>
2022-07-20 20:17:51 +0800

    Added implementation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap, Marker } from '@capacitor/google-maps';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  @ViewChild('map') mapRef: ElementRef<HTMLElement>;
+  newMap: GoogleMap;
+
+  apiKey = '';
+
   constructor() {}
+
+  ionViewDidEnter() {
+    this.createMap();
+  }
+
+  async createMap() {
+    try {
+      this.newMap = await GoogleMap.create({
+        id: 'my-cool-map',
+        element: this.mapRef.nativeElement,
+        apiKey: 'API KEY HERE',
+        config: {
+          center: {
+            lat: 14.553859198304545,
+            lng: 121.04492312876066,
+          },
+          zoom: 8,
+        },
+      });
+    } catch(e) {
+      console.log(e);
+    }
+  }
 
 }
